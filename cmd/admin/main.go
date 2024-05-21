f950e4eb89e6e108eee2ef2c75a30fab9587133c
Oleh Mushka <olegamysk@gmail.com>
2024-05-21 16:13:31 +0300

    NN-8 added stub for admin

---
+++ cmd/admin/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/ecumenos-social/network-node/cmd/admin/configurations"
+	cli "github.com/urfave/cli/v2"
+)
+
+func main() {
+	if err := run(os.Args); err != nil {
+		fmt.Println("exiting", "err", err)
+		os.Exit(-1)
+	}
+}
+
+func run(args []string) error {
+	app := cli.App{
+		Name:    configurations.ServiceName,
+		Usage:   "administration of network node",
+		Version: string(configurations.ServiceVersion),
+		Flags:   []cli.Flag{},
+		Commands: []*cli.Command{
+			runAppCmd,
+		},
+	}
+
+	return app.Run(args)
+}
